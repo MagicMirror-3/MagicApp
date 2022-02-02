1882229fe5c162459b98732c1693b162c98707ff
Simon <stauss.simon.01@gmail.com>
2022-02-02 20:35:21 +0100

    added settings UI framework

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:magic_app/text_types.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -10,23 +10,30 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool darkMode = true;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const DefaultPlatformText("Settings"),
-        PlatformElevatedButton(
-          child: DefaultPlatformText(
-            "Switch platform to ${isMaterial(context) ? "iOS" : "Android"}",
-          ),
-          onPressed: () {
-            isMaterial(context)
-                ? PlatformProvider.of(context)!.changeToCupertinoPlatform()
-                : PlatformProvider.of(context)!.changeToMaterialPlatform();
-          },
+    print("Dark Mode: $darkMode");
+    return SettingsList(
+      applicationType: ApplicationType.both,
+      sections: [
+        SettingsSection(
+          title: const Text("Appearance"),
+          tiles: [
+            SettingsTile.switchTile(
+              leading: Icon(PlatformIcons(context).brightness),
+              initialValue: darkMode,
+              onToggle: (value) {
+                ;
+              },
+              title: const Text("Dark Mode"),
+              description: const Text("Enables the dark mode."),
+            ),
+          ],
         ),
       ],
+      brightness: Brightness.dark,
     );
   }
 }
