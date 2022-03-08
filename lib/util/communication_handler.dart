e9d07a72faa7e87243e917c41ed28e402780e1d6
Simon <stauss.simon.01@gmail.com>
2022-03-08 12:17:52 +0100

    persistent connection and docstrings

--- lib/util/communication_handler.dart
+++ lib/util/communication_handler.dart
@@ -1,27 +1,42 @@
-import 'dart:io';
-
 import 'package:http/http.dart' as http;
 import 'package:magic_app/settings/constants.dart';
 import 'package:magic_app/util/shared_preferences_handler.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 import 'package:network_tools/network_tools.dart';
 
+/// Handles the communication with the MagicController on the Raspberry pi via HTTP.
 class CommunicationHandler {
+  /// The port the mirror backend runs on
   static const int _port = 5000;
 
+  /// Whether a mirror is connected
   static bool _connected = false;
 
+  /// Whether a mirror is connected
   static bool get isConnected => _connected;
-  static InternetAddress? _address;
-  static String? _macAddress;
+
+  /// The IP-address of the mirror
+  static String? _address;
+
+  /// Saves the given address both locally and in the SharedPreferences.
+  static set address(String address) {
+    _address = address;
+    SharedPreferencesHandler.saveValue(
+      SettingKeys.mirrorAddress,
+      address,
+    );
+  }
+
+  /// A (potential) persistent connection to the mirror
   static late http.Client _mirrorClient;
 
+  /// Discovers devices on the local network and returns the IP-addresses of MagicMirrors
   static Future<List<String>> findLocalMirrors() async {
     final String? wifiIP = await NetworkInfo().getWifiIP();
 
     if (wifiIP != null) {
       final String subnet = wifiIP.substring(0, wifiIP.lastIndexOf("."));
-      print("Searching on subnet $subnet");
+      print("Searching on subnet $subnet...");
       final hostStream = HostScanner.discover(subnet);
 
       List<String> mirrorList = [];
@@ -31,17 +46,11 @@ class CommunicationHandler {
         // Check if the desired port is open
         if ((await PortScanner.isOpen(host.ip, _port)).isOpen) {
           // Check if the device has magic mirror routes
-          http.Response response = await http
-              .get(
-                createRouteURI(_MagicRoutes.isMagicMirror, host.ip),
-              )
-              .timeout(
-                const Duration(milliseconds: 500),
-                onTimeout: () => http.Response("timeout", 408),
-              );
+          print("device found at ${host.ip}. Checking routes...");
+          bool isMirror = await isMagicMirror(host.ip);
 
-          if (response.statusCode == 200) {
-            print("device found at ${host.ip}");
+          if (isMirror) {
+            print("This is indeed a mirror!");
             mirrorList.add(host.ip);
           } else {
             print("this device does not have the mirror routes");
@@ -56,45 +65,75 @@ class CommunicationHandler {
     }
   }
 
-  static Future<bool> connectToMirror() async {
-    final String savedAddress =
+  /// Checks whether the given [host] is a MagicMirror.
+  ///
+  /// It calls the route '/isMagicMirror' with a http get request.
+  static Future<bool> isMagicMirror(String host) async {
+    http.Response response = await http
+        .get(
+          createRouteURI(_MagicRoutes.isMagicMirror, host),
+        )
+        .timeout(
+          const Duration(milliseconds: 500),
+          onTimeout: () => http.Response("timeout", 408),
+        );
+
+    return response.statusCode == 200;
+  }
+
+  /// Try connecting to the previously saved IP or discover devices on the network
+  /// potentially being a network.
+  ///
+  /// If multiple mirrors are found, it returns a [List<String>] representing the
+  /// IPs of every mirror in the local network. If only one is found, the [Future]
+  /// returns nothing.
+  static Future<dynamic> connectToMirror() async {
+    String mirrorAddress =
         SharedPreferencesHandler.getValue(SettingKeys.mirrorAddress);
-    if (savedAddress.isNotEmpty) {
-      print("Saved Address: $savedAddress");
-    } else {
+
+    if (mirrorAddress.isNotEmpty) {
+      print("Saved Address: $mirrorAddress");
+      _connected = await isMagicMirror(mirrorAddress);
+    }
+
+    if (mirrorAddress.isEmpty || !_connected) {
       final List<String> foundDevices = await findLocalMirrors();
       print("all devices: $foundDevices");
 
       if (foundDevices.length == 1) {
         print("Thankfully, only one mirror found: ");
-        _mirrorClient = http.Client();
+        _connected = true;
+        mirrorAddress = foundDevices.first;
+      } else {
+        print("somehow make the user choose");
+        return foundDevices;
       }
     }
 
-    _connected = true;
-    // _address = await getIPbyMAC();
-
-    // Send a request to the mirror
-    return true;
+    // Create a persistent client and save the mirror address
+    _mirrorClient = http.Client();
+    address = mirrorAddress;
   }
 
-  static Uri createRouteURI(_MagicRoute route, dynamic host) {
-    if (host == null) {
-      host = _address!;
-    } else {
-      if (host is String) {
-        host = InternetAddress(host, type: InternetAddressType.IPv4);
-      }
-    }
+  /// Creates an URI for a given [route] at the [host].
+  ///
+  /// [_MagicRoutes] contains every supported route.
+  ///
+  /// The [host] param is optional and wil default to the [_address] field of
+  /// this class.
+  static Uri createRouteURI(_MagicRoute route, String? host) {
+    host ??= _address;
 
-    if (host is InternetAddress) {
-      return Uri.parse("http://${host.address}:$_port/${route.route}");
-    } else {
-      throw TypeError();
-    }
+    return Uri.parse("http://$host:$_port/${route.route}");
+  }
+
+  /// Closes the persistent connection to the mirror.
+  static void closeConnection() {
+    _mirrorClient.close();
   }
 }
 
+/// Contains all valid routes a MagicMirror has
 class _MagicRoutes {
   static const _MagicRoute isMagicMirror = _MagicRoute(route: "isMagicMirror");
   static const _MagicRoute createUser = _MagicRoute(
@@ -105,9 +144,13 @@ class _MagicRoutes {
   );
 }
 
+/// Contains information about a route
 class _MagicRoute {
   const _MagicRoute({required this.route, this.params});
 
+  /// The name of the route
   final String route;
+
+  /// Every parameter the request has to contain
   final List<String>? params;
 }
