1ee7911c0844db46f04b2b75f113b162363e35ce
Simon <stauss.simon.01@gmail.com>
2022-03-22 13:59:34 +0100

    user data is shared with the backend

--- lib/user/user_edit.dart
+++ lib/user/user_edit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:magic_app/util/communication_handler.dart';
 import 'package:magic_app/util/text_types.dart';
 import 'package:magic_app/util/utility.dart';
 
@@ -97,6 +98,7 @@ class _UserEditState extends State<UserEdit> {
                             SettingKeys.user,
                             userInfo,
                           );
+                          CommunicationHandler.updateUserData();
                         })
                     : null,
                 padding: const EdgeInsets.all(8),
